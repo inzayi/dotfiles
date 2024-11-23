3ab337e218dde0f32955ab83308f0bbd63df51b0
sjkey <kuba.serwin99@gmail.com>
2024-11-23 11:45:28 +0100

    feat: init script and .zshrc config

---
+++ .config/.zshrc
@@ -0,0 +1,39 @@
+bindkey -v  # Enable vi mode
+
+unset LAST_LOGIN
+
+# NVM Configuration
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# general aliases
+alias c="clear"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ll="ls -la"
+alias mindnest="cd $HOME/Desktop/projects/mindnest"
+
+# pnpm
+alias pi="pnpm install"
+alias pb="pnpm build"
+alias dev="pnpm dev"
+
+# git aliases
+alias gs="git status"
+alias ga="git add ."
+alias gc="git commit -m"
+alias gl="git log --oneline --graph --decorate"
+alias gp="git pull"
+alias gpu="git push"
+alias gb="git branch"
+alias gst="git stash"
+alias gr="git restore ."
+alias grs="git restore --staged ."
+
+# utils
+killport() {
+  lsof -ti tcp:$1 | xargs kill -9
+}
+
+eval "$(starship init zsh)"
---
+++ init.zsh
@@ -0,0 +1,50 @@
+# [homebrew]
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# [pnpm]
+brew install pnpm
+
+# [nvm]
+brew install nvm
+mkdir -p ~/.nvm
+cat << 'EOF' >> ~/.zshrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+EOF
+
+# [node]
+nvm install node
+
+# [font]
+brew install --cask font-dejavu-sans-mono-nerd-font
+
+# [starship]
+brew install starship
+mkdir -p ~/.config
+cp ./.config/starship.toml ~/.config/starship.toml
+
+# [kitty]
+brew install kitty
+mkdir -p ~/.config/kitty
+cp ./.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+cp ./.config/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
+
+# [vscode]
+brew install --cask visual-studio-code
+mkdir -p "$HOME/Library/Application Support/Code/User"
+cp ./.vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+cp ./.vscode/keybindings.json "$HOME/Library/Application Support/Code/User/keybindings.json"
+
+# [raycast]
+brew install --cask raycast
+
+# [docker]
+brew install --cask docker
+
+# --- Desktop Applications ---
+brew install --cask figma
+brew install --cask postman
+brew install --cask slack
+brew install --cask spotify
+brew install --cask zen-browser
