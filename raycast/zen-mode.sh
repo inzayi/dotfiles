3e2dce10b2d04fc4659cfcc5cf5c44079bffc080
yintio <kuba.serwin99@gmail.com>
2025-11-15 09:29:16 +0100

    feat: update zen mode script and add new wallpapers

--- raycast/zen-mode.sh
+++ raycast/zen-mode.sh
@@ -1,4 +1,4 @@
 open -a "Visual Studio Code"
 sleep 0.01
-open -g raycast://extensions/raycast/window-management/center-two-thirds
+open -g raycast://extensions/raycast/window-management/last-two-thirds
 open -g raycast://extensions/raycast/system/hide-all-apps-except-frontmost
